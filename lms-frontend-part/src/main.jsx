1b8d8f43a68329603921de1a9e5501892cf4390d
Hayat <hayatjahangir0@gmail.com>
2024-12-28 23:11:22 +0530

    add react hot toast

--- lms-frontend-part/src/main.jsx
+++ lms-frontend-part/src/main.jsx
@@ -3,12 +3,14 @@ import './index.css';
 import ReactDom from 'react-dom/client';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from'react-hot-toast';
 
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
+    <Toaster/>
   </BrowserRouter>
 
 );
